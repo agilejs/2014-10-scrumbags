8987da00416dc9da9c1305d3966542605fb99940
Gwindorith <wjw.notten@gmail.com>
2014-10-13 15:57:07 +0200

    fixed MORE buildy thingy

--- client/js/app.js
+++ client/js/app.js
@@ -44,10 +44,6 @@ angular.module('MovieDatabase', ['ngRoute', 'textAngular']).config(
         controller: ErrorCtrl,
         templateUrl: '/partial/error.html'
     })
-    .when('/actors', {
-        controller: ActorsAddCtrl,
-        templateUrl: '/partial/actors/add.html'
-    })
     .otherwise({
         redirectTo: function () {
             return '/404?culprit=client';
